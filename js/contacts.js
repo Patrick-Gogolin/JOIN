5528e2c81f6ab4a90d8410d1084871c928c97272
SandorBalazsi <sandor.b@gmx.ch>
2024-06-20 23:29:06 +0200

    worked on contacts js

--- js/contacts.js
+++ js/contacts.js
@@ -19,10 +19,7 @@ async function onloadFunc(){
         
     }
 
-    console.log(contacts);
-    //postData("/contacts", {"":""});
-    //deleteData("/contacts/-O-SvGfTOuulS1WyvDew");
-    //putData("/contacts", {"":""});
+   loadContacts();
 }   
 
 async function loadContacts(path=""){
@@ -33,12 +30,13 @@ async function loadContacts(path=""){
         let eachContact = contacts[index];
 
         document.getElementById('contact-list').innerHTML += getContactListTemplate(eachContact);
-
+        getContactsInitials(eachContact);
     }
 
     return responseToJson = await response.json();
     
 }
+
 
 async function postContacts(path="", data={}){
     let name = document.getElementById('name');
@@ -99,17 +97,72 @@ function doNotClose(event){
 }
 
 function getContactListTemplate(eachContact){
-    return`<div class="contact">
-    <div class="contact-logo">AM</div>
-    <div class="contact-name">
-        <p>${eachContact.contact.name}</p>
-        <a href="">${eachContact.contact.email}</a>
+    let initials = getContactsInitials(eachContact);
+    return`
+    <div id="contact-list-element-${eachContact.id}" class="contact" onclick='showContactInfo(${JSON.stringify(eachContact)})'>
+        <div class="contact-logo">${initials}</div>
+        <div class="contact-name">
+         <p>${eachContact.contact.name}</p>
+         <a href="">${eachContact.contact.email}</a>
     </div>
     </div>`;
+}
+
+function getContactsInitials(eachContact){
+    let fullname = eachContact.contact.name;
+    let nameparts = fullname.split(" ");
+    let initials = "";
+    
+    if (nameparts.length > 1) {
+        initials = nameparts[0].charAt(0).toUpperCase() + nameparts[1].charAt(0).toUpperCase();
+    } else if (nameparts.length === 1){
+        initials = nameparts[0].charAt(0).toUpperCase();
+    }
+    
+    return initials;
 }
 
 function getABCSeparatorTemplate(){
     return `<div class="abc-separator">
                 <p>B</p>
+            </div>`;
+}
+
+function showContactInfo(eachContact){
+
+    document.querySelectorAll('.contact').forEach(element => {
+        element.style.backgroundColor = '';
+        element.style.color = '';
+    })
+
+    document.getElementById(`contact-list-element-${eachContact.id}`).style.backgroundColor = "#2A3647";
+    document.getElementById(`contact-list-element-${eachContact.id}`).style.color = "white";
+    document.getElementById('contact-info').innerHTML = getEachContactInfo(eachContact);
+}
+
+
+function getEachContactInfo(eachContact){
+    let initials = getContactsInitials(eachContact);
+    let contactName = eachContact.contact.name;
+    let contactEmail = eachContact.contact.email;
+    let contactPhone = eachContact.contact.phone;
+
+    return `
+    <div class="contact-data">
+                <div class="contact-data-header">
+                    <div class="contact-data-logo">${initials}</div>
+                    <div class="contact-data-name">${contactName} 
+                        <div class="contact-data-icon">
+                            <div class="edit"><img src="/img/edit.png" alt=""><p>Edit</p></div>
+                            <div class="delete"><img src="/img/delete.png" alt=""><p>Delete</p></div>
+                        </div>
+                    </div>
+                </div>
+                <h2>Contact Information</h2>
+                <h3>Email</h3>
+                <a href="">${contactEmail}</a>
+                <h3>Phone</h3>
+                <p>${contactPhone}</p>
+
             </div>`;
 }
