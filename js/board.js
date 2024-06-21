9d0921fbc2f9fa4f48559de035db104a7665f2ce
Patrick-Gogolin <patrick.gogolin@hotmail.com>
2024-06-21 03:12:31 +0200

    updated Board

---
+++ js/board.js
@@ -0,0 +1,8 @@
+function openAddTaskOverlayer() {
+    document.getElementById('overlayer').classList.remove('d-none');
+}
+
+function closeOverlayer() {
+    document.getElementById('overlayer').classList.add('d-none')
+
+}
