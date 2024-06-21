188b55c3ae6b907c6122eb204f3b096e93707dc5
SandorBalazsi <sandor.b@gmx.ch>
2024-06-21 02:05:31 +0200

    worked on contacts js

--- js/contacts.js
+++ js/contacts.js
@@ -15,22 +15,46 @@ async function onloadFunc(){
                 contact : contactResponse[contactKeysArray[index]],
 
             }
-        )
-        
+        )   
     }
-
-   loadContacts();
+    sortContactsAlphabetically();
+    await loadContacts('/contacts');
 }   
+
+function sortContactsAlphabetically() {
+    contacts.sort((a, b) => {
+        const nameA = a.contact.name.toUpperCase();
+        const nameB = b.contact.name.toUpperCase(); 
+        
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+        return 0;
+    });
+}
+
 
 async function loadContacts(path=""){
     let response = await fetch(BASE_URL + path + ".json");
-    
+    let contactListElement = document.getElementById('contact-list');
+    contactListElement.innerHTML = "";
+    let currentInitial = "";
 
     for (let index = 0; index < contacts.length; index++) {
         let eachContact = contacts[index];
+        let contactInitial = eachContact.contact.name.charAt(0).toUpperCase();
 
-        document.getElementById('contact-list').innerHTML += getContactListTemplate(eachContact);
+        if (contactInitial !== currentInitial) {
+            currentInitial = contactInitial;
+            contactListElement.innerHTML += getABCSeparatorTemplate(currentInitial);
+        }
+
+       contactListElement.innerHTML += getContactListTemplate(eachContact);
         getContactsInitials(eachContact);
+        random_bg_color(eachContact);
     }
 
     return responseToJson = await response.json();
@@ -62,12 +86,14 @@ async function postContacts(path="", data={}){
     return responseToJson = await response.json();
 }
 
+
 async function deleteContacts(path=""){
     let response = await fetch(BASE_URL + path + ".json",{
         method: "DELETE",
     });
     return responseToJson = await response.json();
 }
+
 
 async function putContacts(path="", data={}){
     let response = await fetch(BASE_URL + path + ".json",{
@@ -80,33 +106,39 @@ async function putContacts(path="", data={}){
     return responseToJson = await response.json();
 }
 
+
 async function editContact(){
     putContacts(`contacts/${id}`, user);
 }
+
 
 function addContact(){
     document.getElementById('add-contact-popup').classList.remove("d-none");
 }
 
+
 function closePopup(){
     document.getElementById('add-contact-popup').classList.add("d-none");
 }
+
 
 function doNotClose(event){
     event.stopPropagation();
 }
 
+
 function getContactListTemplate(eachContact){
     let initials = getContactsInitials(eachContact);
     return`
     <div id="contact-list-element-${eachContact.id}" class="contact" onclick='showContactInfo(${JSON.stringify(eachContact)})'>
-        <div class="contact-logo">${initials}</div>
+        <div id="contact-logo-${eachContact.id}" class="contact-logo">${initials}</div>
         <div class="contact-name">
          <p>${eachContact.contact.name}</p>
          <a href="">${eachContact.contact.email}</a>
     </div>
     </div>`;
 }
+
 
 function getContactsInitials(eachContact){
     let fullname = eachContact.contact.name;
@@ -122,11 +154,13 @@ function getContactsInitials(eachContact){
     return initials;
 }
 
-function getABCSeparatorTemplate(){
+
+function getABCSeparatorTemplate(letter){
     return `<div class="abc-separator">
-                <p>B</p>
+                <p>${letter}</p>
             </div>`;
 }
+
 
 function showContactInfo(eachContact){
 
@@ -146,12 +180,20 @@ function getEachContactInfo(eachContact){
     let contactName = eachContact.contact.name;
     let contactEmail = eachContact.contact.email;
     let contactPhone = eachContact.contact.phone;
+    let actualBgColor = document.getElementById(`contact-logo-${eachContact.id}`).style.background;
+    
+    console.log(actualBgColor);
+    
 
     return `
-    <div class="contact-data">
-                <div class="contact-data-header">
-                    <div class="contact-data-logo">${initials}</div>
-                    <div class="contact-data-name">${contactName} 
+    <div class="contact-info-header">
+                <h1>Contacts</h1>
+                <div class="contact-info-header-separator"></div>
+                <span>Better with a team</span>
+                </div>
+                <div class="contact-data">
+                <div id="contact-data-logo" class="contact-data-logo" style="background:${actualBgColor};">${initials}</div>
+                <div class="contact-data-name">${contactName} 
                         <div class="contact-data-icon">
                             <div class="edit"><img src="/img/edit.png" alt=""><p>Edit</p></div>
                             <div class="delete"><img src="/img/delete.png" alt=""><p>Delete</p></div>
@@ -163,6 +205,20 @@ function getEachContactInfo(eachContact){
                 <a href="">${contactEmail}</a>
                 <h3>Phone</h3>
                 <p>${contactPhone}</p>
-
+                
             </div>`;
+}
+
+
+function random_bg_color(eachContact) {
+    // Generate random values for red, green, and blue components between 0 and 255.
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    // Construct the RGB color string.
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    // Output the generated color to the console.
+    console.log(bgColor);
+    // Set the background color of the document body to the generated color.
+    document.getElementById(`contact-logo-${eachContact.id}`).style.background = bgColor;
 }
