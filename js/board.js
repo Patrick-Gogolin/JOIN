03c3b43e2eb68c872790c5926327e3a8565c246f
Patrick-Gogolin <patrick.gogolin@hotmail.com>
2024-06-25 12:16:08 +0200

    updated addTaskSlide Prio Buttons

--- js/board.js
+++ js/board.js
@@ -48,44 +48,74 @@ function checkField(field, requiredSpan) {
     }
 }
 
-function urgentPriority () {
-    let urgentButton = document.getElementById('urgent-button');
-    let urgentPrioSign = document.getElementById('urgent-prio-sign');
-    if(urgentActive === false){
+
+function urgentPriority() {
+    if (!urgentActive) {
+        resetButtons();
+        let urgentButton = document.getElementById('urgent-button');
+        let urgentPrioSign = document.getElementById('urgent-prio-sign');
         urgentButton.classList.add('urgent');
-        urgentPrioSign.src= 'img/urgent-prio-white.svg';
+        urgentPrioSign.src = 'img/urgent-prio-white.svg';
         urgentActive = true;
-    }else{
+    } else {
+        let urgentButton = document.getElementById('urgent-button');
+        let urgentPrioSign = document.getElementById('urgent-prio-sign');
         urgentButton.classList.remove('urgent');
-        urgentPrioSign.src= 'img/urgent-prio.svg';
+        urgentPrioSign.src = 'img/urgent-prio.svg';
         urgentActive = false;
     }
 }
 
-function mediumPriority () {
-    let mediumButton = document.getElementById('medium-button');
-    let mediumPrioSign = document.getElementById('medium-prio-sign');
-    if(mediumActive === false){
+function mediumPriority() {
+    if (!mediumActive) {
+        resetButtons();
+        let mediumButton = document.getElementById('medium-button');
+        let mediumPrioSign = document.getElementById('medium-prio-sign');
         mediumButton.classList.add('medium');
-        mediumPrioSign.src= 'img/medium-prio.svg';
+        mediumPrioSign.src = 'img/medium-prio.svg';
         mediumActive = true;
-    }else{
+    } else {
+        let mediumButton = document.getElementById('medium-button');
+        let mediumPrioSign = document.getElementById('medium-prio-sign');
         mediumButton.classList.remove('medium');
-        mediumPrioSign.src= 'img/medium-prio-orange.svg';
+        mediumPrioSign.src = 'img/medium-prio-orange.svg';
         mediumActive = false;
     }
 }
 
-function lowPriority () {
-    let lowButton = document.getElementById('low-button');
-    let lowPrioSign = document.getElementById('low-prio-sign');
-    if(lowActive === false){
+function lowPriority() {
+    if (!lowActive) {
+        resetButtons();
+        let lowButton = document.getElementById('low-button');
+        let lowPrioSign = document.getElementById('low-prio-sign');
         lowButton.classList.add('low');
-        lowPrioSign.src= 'img/low-prio-white.svg';
+        lowPrioSign.src = 'img/low-prio-white.svg';
         lowActive = true;
-    }else{
+    } else {
+        let lowButton = document.getElementById('low-button');
+        let lowPrioSign = document.getElementById('low-prio-sign');
         lowButton.classList.remove('low');
-        lowPrioSign.src= 'img/low-prio.svg';
+        lowPrioSign.src = 'img/low-prio.svg';
         lowActive = false;
     }
+}
+
+function resetButtons() {
+    let urgentButton = document.getElementById('urgent-button');
+    let urgentPrioSign = document.getElementById('urgent-prio-sign');
+    urgentButton.classList.remove('urgent');
+    urgentPrioSign.src= 'img/urgent-prio.svg';
+    urgentActive = false; 
+
+    let mediumButton = document.getElementById('medium-button');
+    let mediumPrioSign = document.getElementById('medium-prio-sign');
+    mediumButton.classList.remove('medium');
+    mediumPrioSign.src= 'img/medium-prio-orange.svg';
+    mediumActive = false;
+
+    let lowButton = document.getElementById('low-button');
+    let lowPrioSign = document.getElementById('low-prio-sign');
+    lowButton.classList.remove('low');
+    lowPrioSign.src= 'img/low-prio.svg';
+    lowActive = false;
 }
