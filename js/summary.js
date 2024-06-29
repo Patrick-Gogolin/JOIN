930dace8546c5b6180c838f091e4a4feaa347e18
Patrick-Gogolin <patrick.gogolin@hotmail.com>
2024-06-29 11:44:59 +0200

    updated Summary

--- js/summary.js
+++ js/summary.js
@@ -10,6 +10,37 @@
             } else if (hours >= 18) {
                 greeting = 'Good evening,';
             }
+            init();
 
             greetingElement.textContent = greeting;
+        }
+
+        function load() {
+            let userAsText = localStorage.getItem('user');
+            if (userAsText) {
+                let user = JSON.parse(userAsText);
+                console.log(user);
+                return user; // User zurückgeben
+            } else {
+                console.log("user not found");
+                return null; // Null zurückgeben, wenn kein Benutzer gefunden wird
+            }
+        }
+
+        function init() {
+            let userName = document.getElementById('greeting-name');
+            let user = load(); // Benutzer aus der Load-Funktion abrufen
+            if (user) {
+                let name = user['name'];
+                let surname = user['surname'];
+                userName.innerHTML = showUserInitials(name, surname); // Initialen anzeigen
+            }
+            else{
+                userName.innerHTML = "Guest";
+            }
+        }
+
+
+        function showUserInitials(name, surname) {
+            return `${name} ${surname}`; // Initialen zurückgeben
         }
