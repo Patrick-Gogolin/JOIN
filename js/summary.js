f144c8e6616c58311fbba8d660a0aaa04d7e1b80
PatrickKanert <patrickkanert@gmail.com>
2024-07-06 11:29:24 +0200

    updated summary site

--- js/summary.js
+++ js/summary.js
@@ -1,46 +1,50 @@
-        function updateGreeting() {
-            const greetingElement = document.getElementById('greeting-text');
-            const now = new Date();
-            const hours = now.getHours();
+function updateGreeting() {
+    const greetingElement = document.getElementById('greeting-text');
+    const now = new Date();
+    const hours = now.getHours();
 
-            let greeting = 'Good morning,';
+    let greeting = 'Good morning';
 
-            if (hours >= 12 && hours < 18) {
-                greeting = 'Good afternoon,';
-            } else if (hours >= 18) {
-                greeting = 'Good evening,';
-            }
-            initUserName();
+    if (hours >= 12 && hours < 18) {
+        greeting = 'Good afternoon';
+    } else if (hours >= 18) {
+        greeting = 'Good evening';
+    }
 
-            greetingElement.textContent = greeting;
-        }
+    const user = loadUserName();
 
-        function loadUserName() {
-            let userAsText = localStorage.getItem('user');
-            if (userAsText) {
-                let user = JSON.parse(userAsText);
-                console.log(user);
-                return user; // User zurückgeben
-            } else {
-                console.log("user not found");
-                return null; // Null zurückgeben, wenn kein Benutzer gefunden wird
-            }
-        }
+    if (user && user.name.toLowerCase() !== 'guest') {
+        greeting += ',';
+    }
 
-        function initUserName() {
-            let userName = document.getElementById('greeting-name');
-            let user = load(); // Benutzer aus der Load-Funktion abrufen
-            if (user) {
-                let name = user['name'];
-                let surname = user['surname'];
-                userName.innerHTML = showUserName(name, surname); // Initialen anzeigen
-            }
-            else{
-                userName.innerHTML = "Guest";
-            }
-        }
+    greetingElement.textContent = greeting;
+    initUserName();
+}
 
+function loadUserName() {
+    let userAsText = localStorage.getItem('user');
+    if (userAsText) {
+        let user = JSON.parse(userAsText);
+        console.log(user);
+        return user; // User zurückgeben
+    } else {
+        console.log("user not found");
+        return null; // Null zurückgeben, wenn kein Benutzer gefunden wird
+    }
+}
 
-        function showUserName(name, surname) {
-            return `${name} ${surname}`; // Initialen zurückgeben
-        }
+function initUserName() {
+    let userNameElement = document.getElementById('greeting-name');
+    let user = loadUserName(); // Benutzer aus der Load-Funktion abrufen
+    if (user) {
+        let name = user['name'];
+        let surname = user['surname'];
+        userNameElement.innerHTML = showUserName(name, surname); // Namen anzeigen
+    } else {
+        userNameElement.innerHTML = "";
+    }
+}
+
+function showUserName(name, surname) {
+    return `${name} ${surname}`; // Namen zurückgeben
+}
