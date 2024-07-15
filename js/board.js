7f3f3c0270b69a38d87532ec328401f163506bac
SandorBalazsi <166751447+SandorBalazsi@users.noreply.github.com>
2024-07-15 23:54:09 +0200

    board responsiveness updated

--- js/board.js
+++ js/board.js
@@ -377,3 +377,39 @@ function renderTasks(tasks, status) {
 
     removeHighlight(status);
 }
+
+function enableDrawing() {
+    const sliders = document.querySelectorAll('.rendered-tasks-area-to-do, .rendered-tasks-area-in-progress, .rendered-tasks-area-await-feedback, .rendered-tasks-area-done');
+    
+    sliders.forEach(slider => {
+        let isDown = false;
+        let startX;
+        let scrollLeft;
+
+        slider.addEventListener('mousedown', (e) => {
+            isDown = true;
+            slider.classList.add('active');
+            startX = e.pageX - slider.offsetLeft;
+            scrollLeft = slider.scrollLeft;
+        });
+
+        slider.addEventListener('mouseleave', () => {
+            isDown = false;
+            slider.classList.remove('active');
+        });
+
+        slider.addEventListener('mouseup', () => {
+            isDown = false;
+            slider.classList.remove('active');
+        });
+
+        slider.addEventListener('mousemove', (e) => {
+            if (!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - slider.offsetLeft;
+            const walk = (x - startX);
+            slider.scrollLeft = scrollLeft - walk;
+            console.log(walk);
+        });
+    });
+}
