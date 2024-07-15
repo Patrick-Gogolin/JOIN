73307255eef246a9f63d9f6ea0234a4a3beceef1
Patrick-Gogolin <patrick.gogolin@hotmail.com>
2024-07-15 19:28:02 +0200

    updateHTML Function made shorter

--- js/board-template.js
+++ js/board-template.js
@@ -66,3 +66,165 @@ function renderDetailTaskSlideNotDoneSubtasksHtml(subtask) {
         </div>
     </div>`; 
 }
+
+function renderEmptyTasksInToDoHtml() {
+    return /*html*/`
+    <div class="nothing-to-do-nothing-done-container">
+        <span>No tasks To do</span>
+    </div>`;
+}
+
+function renderTasksInToDoHtml(task, bgColor, subtasks, doneSubtasks, imageSrc, i) {
+    return /*html*/`
+    <div onclick="renderDetailTaskSlide('${task.id}')" draggable="true" ondragstart="startDragging('${task.id}')" onclick="editTask(${i})" id="${task.id}" class="task-container">
+        <div class="category-container ${bgColor}">
+            <span class="category-span" id="category${i}">${task.category}</span>
+        </div>
+        <div class="title-container">
+            <span class="title-span" id="title${i}">${task.title}</span>
+        </div>
+        <div class="description-container">
+            <p id="description${i}">${task.description}</p> 
+        </div>
+        <div class="subtasks-container">
+            <label for="file">${doneSubtasks}/${subtasks} Subtasks</label>
+            <progress id="file" value=${doneSubtasks} max=${subtasks}> 1 </progress>
+        </div>
+        <div class="contacts-and-priority-container">
+            <div id="contacts-todo-container${i}" class="contacts-container">
+
+            </div>
+            <div id="priority-container${i}" class="priority-container">
+                <img src=${imageSrc} alt="">
+            </div>   
+        </div>
+    </div>`;
+}
+
+function renderAssignedContactsInToDo(initial, contactColors) {
+   return /*html*/`
+   <div class="rendered-task-assigned-contact-container" style="background-color:${contactColors}">
+        <span>${initial}
+    </div>`;
+}
+
+function renderEmptyTasksInToInProgressHtml() {
+    return /*html*/`
+    <div class="nothing-to-do-nothing-done-container">
+        <span>No tasks in Progress</span>
+    </div>`;
+}
+
+function renderTasksInProgressHtml(task, bgColor, subtasks, doneSubtasks, imageSrc, i) {
+    return /*html*/`
+    <div onclick="renderDetailTaskSlide('${task.id}')" draggable="true" ondragstart="startDragging('${task.id}')" onclick="editTask(${i})" id="${task.id}" class="task-container">
+        <div class="category-container ${bgColor}">
+            <span class="category-span" id="category${i}">${task.category}</span>
+        </div>
+        <div class="title-container">
+            <span class="title-span" id="title${i}">${task.title}</span>
+        </div>
+        <div class="description-container">
+            <p id="description${i}">${task.description}</p> 
+        </div>
+        <div class="subtasks-container">
+            <label for="file">${doneSubtasks}/${subtasks} Subtasks</label>
+            <progress id="file" value=${doneSubtasks} max=${subtasks}> 1 </progress>
+        </div>
+        <div class="contacts-and-priority-container">
+            <div id="contacts-progress-container${i}" class="contacts-container">
+            </div>
+            <div id="priority-container${i}" class="priority-container">
+                <img src=${imageSrc} alt="">
+            </div>   
+        </div>
+    </div>`;
+}
+
+function renderAssignedContactsInProgress(initial, contactColors) {
+    return /*html*/`
+    <div class="rendered-task-assigned-contact-container" style="background-color:${contactColors}">
+         <span>${initial}
+     </div>`;
+ }
+
+ function renderEmptyTasksInToAwaitFeedbackHtml() {
+    return /*html*/`
+    <div class="nothing-to-do-nothing-done-container">
+        <span>No tasks waiting for Feedback</span>
+    </div>`;
+}
+
+ function renderTasksInFeedbackHtml(task, bgColor, subtasks, doneSubtasks, imageSrc, i) {
+    return /*html*/`
+    <div onclick="renderDetailTaskSlide('${task.id}')" draggable="true" ondragstart="startDragging('${task.id}')" onclick="editTask(${i})" id="${task.id}" class="task-container">
+        <div class="category-container ${bgColor}">
+            <span class="category-span" id="category${i}">${task.category}</span>
+        </div>
+        <div class="title-container">
+            <span class="title-span" id="title${i}">${task.title}</span>
+        </div>
+        <div class="description-container">
+            <p id="description${i}">${task.description}</p> 
+        </div>
+        <div class="subtasks-container">
+            <label for="file">${doneSubtasks}/${subtasks} Subtasks</label>
+            <progress id="file" value=${doneSubtasks} max=${subtasks}> 1 </progress>
+        </div>
+        <div class="contacts-and-priority-container">
+            <div id="contacts-feedback-container${i}" class="contacts-container">
+            </div>
+            <div id="priority-container${i}" class="priority-container">
+                <img src=${imageSrc} alt="">
+            </div>   
+        </div>
+    </div>`;
+ }
+
+ function renderAssignedContactsInFeedback(initial, contactColors) {
+    return /*html*/`
+    <div class="rendered-task-assigned-contact-container" style="background-color:${contactColors}">
+        <span>${initial}
+    </div>`;
+ }
+
+ function renderEmptyTasksInToDoneHtml() {
+    return /*html*/`
+    <div class="nothing-to-do-nothing-done-container">
+        <span>No tasks Done</span>
+    </div>`;
+}
+
+function renderTasksInDoneHtml(task, bgColor, subtasks, doneSubtasks, imageSrc, i) {
+    return /*html*/`
+    <div onclick="renderDetailTaskSlide('${task.id}')" draggable="true" ondragstart="startDragging('${task.id}')" onclick="editTask(${i})" id="${task.id}" class="task-container">
+        <div class="category-container ${bgColor}">
+            <span class="category-span" id="category${i}">${task.category}</span>
+        </div>
+        <div class="title-container">
+            <span class="title-span" id="title${i}">${task.title}</span>
+        </div>
+        <div class="description-container">
+            <p id="description${i}">${task.description}</p> 
+        </div>
+        <div class="subtasks-container">
+            <label for="file">${doneSubtasks}/${subtasks} Subtasks</label>
+            <progress id="file" value=${doneSubtasks} max=${subtasks}> 1 </progress>
+        </div>
+            <div class="contacts-and-priority-container">
+                <div id="contacts-done-container${i}" class="contacts-container">
+
+                </div>
+                <div id="priority-container${i}" class="priority-container">
+                    <img src=${imageSrc} alt="">
+                </div>   
+            </div>
+        </div>`;
+}
+
+function renderAssignedContactsInDone(initial, contactColors) {
+    return /*html*/`
+    <div class="rendered-task-assigned-contact-container" style="background-color:${contactColors}">
+        <span>${initial}
+    </div>`;
+    }
