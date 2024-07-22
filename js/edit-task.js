83708ede1571d65fbea27ecc904a149afc9ae724
Patrick-Gogolin <patrick.gogolin@hotmail.com>
2024-07-22 02:28:40 +0200

    new commit

--- js/edit-task.js
+++ js/edit-task.js
@@ -4,6 +4,7 @@ let mediumActiveEditTask = false;
 let lowActiveEditTask = false;
 let assignOptionsContactsContainerEditTask = false;
 let assignedContactsInitialsEditTask = [];
+let editMenuSubtaskIsOpenInEditTask = false;
 
 function OpenEditTaskWindow(id) {
     closeEditTaskOverlay('edit-task-overlayer');
@@ -21,16 +22,16 @@ function OpenEditTaskWindow(id) {
         <img onclick="closeEditTaskOverlay('edit-task-popup')" class="close-overlayer-sign-edit-task" src="/img/x.png" alt="Cross">
         <div class="title-input-container">
                 <span class="headline-input">Title<span class="red-star-required">*</span></span>
-                <input id="title-edit-task" type="text" value="${task.title}" required>
+                <input id="title-edit-task" onblur="newTitle()" type="text" value="${task.title}" required>
                 <span id="title-required-span-edit-task" class="this-field-required-span d-none">This field is required</span>
         </div>
         <div class="description-input-container">
         <span class="headline-input">Description</span>
-        <textarea id="description-of-task-edit-task" class="task-description-textarea" name="" id="">${task.description}</textarea>
+        <textarea id="description-of-task-edit-task" onblur="newDescription()" class="task-description-textarea" name="" id="">${task.description}</textarea>
         </div>
         <div class="date-container">
             <span class="headline-input">Due Date<span class="red-star-required">*</span></span>
-            <input id="date-edit-task" type="date" value=${task.deadline} required>
+            <input id="date-edit-task" onblur="newDate()" type="date" value=${task.deadline} required>
             <span id="date-required-span-edit-task" class="this-field-required-span d-none">This field is required</span>
         </div>
         <div class="priority-container">
@@ -77,23 +78,27 @@ function OpenEditTaskWindow(id) {
             <div class="add-subtask-container">
                 <span class="headline-input">Subtask</span>
                 <div class="add-subtask-input-container">
-                    <input id="add-subtask-input-container-inputfield" oninput="changeIcons()" type="text" placeholder="Add new subtask">
-                    <div id="add-subtask-svg-container" class="add-subtask-svg-container">
+                    <input id="add-subtask-input-container-inputfield-edit-task" oninput="changeIconsSubtasksEditTask()" type="text" placeholder="Add new subtask">
+                    <div id="add-subtask-svg-container-edit-task" class="add-subtask-svg-container">
                         <img id="add-subtask-svg" class="add-subtask-svg" src="img/addsubtask.svg" alt="">
                     </div>
-                    <div id="cancel-or-confirm-subtask-container" class="cancel-or-confirm-subtask-container d-none">
-                        <img onclick="clearSubtask()" src="img/cancel.svg" alt="">
+                    <div id="cancel-or-confirm-subtask-container-edit-task" class="cancel-or-confirm-subtask-container d-none">
+                        <img onclick="clearSubtaskEditTask()" src="img/cancel.svg" alt="">
                         <div class="seperator"></div>
-                        <img onclick="addSubtask()" src="img/check-grey.svg" alt="">
+                        <img onclick="addSubtaskEditTask()" src="img/check-grey.svg" alt="">
                     </div>
                 </div>
             </div>
-            <div id="added-subtask-main-container" class="added-subtask-main-container">
+            <div id="added-subtask-main-container-edit-task" class="added-subtask-main-container">
             </div>
+        </div>
+        <div class="update-task-button-container">
+            <button onclick= "updateTask()">Ok <img src="img/check.svg" alt=""></button>
         </div>
     </div>`;
 
     renderAssignedContactsEditTask();
+    renderSubtasksFromEditTask();
 }
 
 function getPrioritySvgPaths(priority) {
@@ -255,7 +260,7 @@ function renderContactsEditTask() {
             let checkBox = renderCheckBoxEditTask(activeUserUpdated);
             let bgColor = emptyTask.assignedContacts.indexOf(activeUserUpdated) !== -1 ? 'bg-navy' : 'bg-white';
             activeUserContainer.innerHTML = /*html*/`
-            <div id="logged-in-user-edit-task" onclick="assignTaskToLoggedInUser('logged-in-user')" class="single-contact-container ${bgColor}">
+            <div id="logged-in-user-edit-task" onclick="assignTaskToLoggedInUserEditTask('logged-in-user-edit-task')" class="single-contact-container ${bgColor}">
                 <div class="single-contact-name-container">
                     <div class="contact-name-initials-container" style="background-color: ${colorForActiveUser[0]};">
                         <span class="user-initials-span">${activeUserInitialsUpdated}</span>
@@ -298,7 +303,7 @@ function renderContactsEditTask() {
             let checkBoxContacts = renderCheckBoxEditTask(userName);
             let bgColor = emptyTask.assignedContacts.indexOf(userName) !== -1 ? 'bg-navy' : 'bg-white'
             container.innerHTML += /*html*/`
-     <div id="${i}" onclick="assignTaskToContact(${i})"  class="single-contact-container ${bgColor}">
+     <div id="${i}" onclick="assignTaskToContactEditTask(${i})"  class="single-contact-container ${bgColor}">
         <div class="single-contact-name-container">
             <div class="contact-name-initials-container" style="background-color: ${color};">
                 <span class="user-initials-span">${userNameInitial}</span>
@@ -349,6 +354,27 @@ function renderCheckBoxEditTask(id) {
     }
 }
 
+function assignTaskToLoggedInUserEditTask(i) {
+    let container = document.getElementById(i);
+    let loggedInUserName = document.getElementById('logged-in-user-name-edit-task').innerHTML;
+    let index = emptyTask.assignedContacts.indexOf(loggedInUserName);
+    let checkbox = document.getElementById('checkbox-active-user-edit-task');
+    if (index === -1) {
+        container.classList.add('bg-navy');
+        emptyTask.assignedContactsColors.push(colorForActiveUser[0]);
+        emptyTask.assignedContacts.push(loggedInUserName);
+        console.log(emptyTask);
+        checkbox.src = "img/filled-check-box-white.svg"
+    }
+    else {
+        container.classList.remove('bg-navy');
+        emptyTask.assignedContacts.splice(index,1);
+        emptyTask.assignedContactsColors.splice(index, 1);
+        console.log(emptyTask);
+        checkbox.src = "img/empty-check-box.svg"
+    }
+}
+
 function assignTaskToContactEditTask(i) {
     let container = document.getElementById(i);
     let contactName = document.getElementById(`assigned-contact-name-edit-task${i}`).innerHTML;
@@ -358,22 +384,216 @@ function assignTaskToContactEditTask(i) {
     if (index === -1) {
         container.classList.add('bg-navy');
         emptyTask.assignedContactsColors.push(colors[i]);
+        emptyTask.assignedContacts.push(contactName);
         console.log(emptyTask);
-        assignedContactsId.push(i);
-        assignedContactsNames.push(contactName)
-        console.log(assignedContactsId);
-        console.log(assignedContactsNames);
-        console.log(assignedContactsColors);
         checkbox.src = "img/filled-check-box-white.svg"
     }
     else {
         container.classList.remove('bg-navy');
-        assignedContactsColors.splice(index,1);
-        assignedContactsNames.splice(index, 1);
-        assignedContactsId.splice(index, 1);
-        console.log(assignedContactsId);
-        console.log(assignedContactsNames);
-        console.log(assignedContactsColors);
+        emptyTask.assignedContacts.splice(index,1);
+        emptyTask.assignedContactsColors.splice(index, 1);
+        console.log(emptyTask);
         checkbox.src = "img/empty-check-box.svg";
     }
+}
+
+function renderSubtasksFromEditTask() {
+    let content = document.getElementById('added-subtask-main-container-edit-task');
+    let subtasks = emptyTask.subtasks;
+    content.innerHTML = "";
+    for (let i = 0; i < subtasks.length; i++) {
+        const subtask = subtasks[i];
+        let index = emptyTask.doneSubtasks.indexOf(subtask)
+        if(index === -1) {
+            content.innerHTML += /*html*/`
+            <div id="single-subtask-container-edit-task${i}" class="single-subtask-container bg-grey-hover" onmouseout="hoverExitFunctionEditTask(${i})" onmouseover="hoverFunctionEditTask(${i})">
+                <ul>
+                    <li id="list-content-edit-task${i}" class="list-element">${subtask}</li>
+                </ul>
+                <div class="checkbox-subtask"><img id="checkbox-subtask-edit-task${i}" src="/img/empty-check-box.svg" alt=""></div>
+                <div id="edit-delete-created-subtask-container-edit-task${i}" class="edit-delete-created-subtask-container d-none">
+                    <img id="single-subtask-edit-edit-task${i}" class="edit-sign" onclick="OpenEditTaskInEditTask(${i})" src="img/edit.svg" alt="">
+                    <div class="seperator"></div>
+                    <img id="single-subtask-delete-edit-task${i}" class="delete-sign" onclick="deleteTaskInEditTask(${i})" src="img/delete.svg" alt="">
+                    <div onclick="changeSubtaskStatus(${i})" class="checkbox-subtask"><img id="checkbox-subtask-edit-task-clickable${i}" src="/img/empty-check-box.svg" alt=""></div>
+                </div>
+            </div>`;
+        }
+        else {
+            content.innerHTML += /*html*/`
+            <div id="single-subtask-container-edit-task${i}" class="single-subtask-container bg-grey-hover" onmouseout="hoverExitFunctionEditTask(${i})" onmouseover="hoverFunctionEditTask(${i})">
+                <ul>
+                    <li id="list-content-edit-task${i}" class="list-element">${subtask}</li>
+                </ul>
+                <div class="checkbox-subtask"><img id="checkbox-subtask-edit-task${i}" src="/img/filled-check-box.svg" alt=""></div>
+                <div id="edit-delete-created-subtask-container-edit-task${i}" class="edit-delete-created-subtask-container d-none">
+                    <img id="single-subtask-edit-edit-task${i}" class="edit-sign" onclick="OpenEditTaskInEditTask(${i})" src="img/edit.svg" alt="">
+                    <div class="seperator"></div>
+                    <img id="single-subtask-delete-edit-task${i}" class="delete-sign" onclick="deleteTaskInEditTask(${i})" src="img/delete.svg" alt="">
+                    <div onclick="changeSubtaskStatus(${i})" class="checkbox-subtask"><img id="checkbox-subtask-edit-task-clickable${i}" src="/img/filled-check-box.svg" alt=""></div>
+                </div>
+            </div>`;
+        }
+        
+    }
+}
+
+function hoverFunctionEditTask(i) {
+    let editContainer = document.getElementById(`edit-delete-created-subtask-container-edit-task${i}`);
+    let checkBox = document.getElementById(`checkbox-subtask-edit-task${i}`)
+    if (editContainer) {
+        editContainer.classList.remove('d-none');
+        checkBox.classList.add('d-none');
+    }
+}
+
+function hoverExitFunctionEditTask(i) {
+    let editContainer = document.getElementById(`edit-delete-created-subtask-container-edit-task${i}`);
+    let checkBox = document.getElementById(`checkbox-subtask-edit-task${i}`)
+    if (editContainer) {
+        editContainer.classList.add('d-none');
+        checkBox.classList.remove('d-none');
+    }
+}
+
+function OpenEditTaskInEditTask(i) {
+    let content = document.getElementById(`single-subtask-container-edit-task${i}`);
+    let listValue = document.getElementById(`list-content-edit-task${i}`).innerHTML;
+
+    if (editMenuSubtaskIsOpenInEditTask === false) {
+        content.classList.remove('bg-grey-hover');
+        content.classList.add('blue-underline');
+        content.innerHTML = /*html*/`
+        <div class="edit-subtask-container">
+                <input id="edit-task-input-edit-task${i}" class="edit-subtask-inputfield" value="${listValue}">
+                <div id="delete-confirm-edit-subtask-edit-task" class="delete-confirm-edit-subtask-container">
+                    <img class="delete-sign" onclick="deleteOpenEditTaskInEditTask(${i})" src="img/delete.svg" alt="">
+                    <div class="seperator"></div>
+                    <img class="confirm-edit" onclick="editTaskInEditTask(${i})" src="img/check-grey.svg" alt="">
+                </div>
+        </div>`;
+        editMenuSubtaskIsOpenInEditTask = true;
+    }
+}
+
+function editTaskInEditTask(i) {
+    let task = document.getElementById(`edit-task-input-edit-task${i}`);
+    emptyTask.subtasks.splice(i, 1, task.value);
+    renderSubtasksFromEditTask();
+    console.log(emptyTask);
+    editMenuSubtaskIsOpenInEditTask = false;
+}
+
+function deleteOpenEditTaskInEditTask(i) {
+    emptyTask.subtasks.splice(i, 1);
+    renderSubtasksFromEditTask();
+    editMenuSubtaskIsOpenInEditTask = false;
+}
+
+function deleteTaskInEditTask(i) {
+    if (editMenuSubtaskIsOpenInEditTask === false) {
+        emptyTask.subtasks.splice(i, 1);
+        renderSubtasksFromEditTask();
+    }
+}
+
+function changeIconsSubtasksEditTask() {
+    let content = document.getElementById('add-subtask-svg-container-edit-task');
+    let cancelAndConfirm = document.getElementById('cancel-or-confirm-subtask-container-edit-task')
+    let input = document.getElementById('add-subtask-input-container-inputfield-edit-task');
+    if (input.value === '') {
+        content.classList.remove('d-none');
+        cancelAndConfirm.classList.add('d-none');
+    }
+    else {
+        content.classList.add('d-none');
+        cancelAndConfirm.classList.remove('d-none');
+    }
+}
+
+function clearSubtaskEditTask() {
+    let addSignContainer = document.getElementById('add-subtask-svg-container-edit-task');
+    let cancelAndConfirm = document.getElementById('cancel-or-confirm-subtask-container-edit-task')
+    let inputField = document.getElementById('add-subtask-input-container-inputfield-edit-task');
+
+    inputField.value = '';
+    addSignContainer.classList.remove('d-none');
+    cancelAndConfirm.classList.add('d-none');
+}
+
+function addSubtaskEditTask() {
+    let addSignContainer = document.getElementById('add-subtask-svg-container-edit-task');
+    let cancelAndConfirm = document.getElementById('cancel-or-confirm-subtask-container-edit-task')
+    let inputField = document.getElementById('add-subtask-input-container-inputfield-edit-task');
+    let content = document.getElementById('added-subtask-main-container-edit-task');
+    let subtasks = emptyTask.subtasks;
+    emptyTask.subtasks.push(inputField.value);
+    content.innerHTML = "";
+    for (let i = 0; i < subtasks.length; i++) {
+        const subtask = subtasks[i];
+        content.innerHTML += /*html*/`
+          <div id="single-subtask-container-edit-task${i}" class="single-subtask-container bg-grey-hover" onmouseout="hoverExitFunctionEditTask(${i})" onmouseover="hoverFunctionEditTask(${i})">
+            <ul>
+                <li id="list-content-edit-task${i}" class="list-element">${subtask}</li>
+            </ul>
+            <div class="checkbox-subtask"><img id="checkbox-subtask-edit-task${i}" src="/img/empty-check-box.svg" alt=""></div>
+            <div id="edit-delete-created-subtask-container-edit-task${i}" class="edit-delete-created-subtask-container d-none">
+                <img id="single-subtask-edit-edit-task${i}" class="edit-sign" onclick="OpenEditTaskInEditTask(${i})" src="img/edit.svg" alt="">
+                <div class="seperator"></div>
+                <img id="single-subtask-delete-edit-task${i}" class="delete-sign" onclick="deleteTaskInEditTask(${i})" src="img/delete.svg" alt="">
+                <div onclick="changeSubtaskStatus(${i})" class="checkbox-subtask"><img id="checkbox-subtask-edit-task-clickable${i}" src="/img/empty-check-box.svg" alt=""></div>
+            </div>
+        </div>`;
+    }
+    inputField.value = '';
+    addSignContainer.classList.remove('d-none');
+    cancelAndConfirm.classList.add('d-none');
+    editMenuSubtaskIsOpenInEditTask = false;
+}
+
+function changeSubtaskStatus(i) {
+    let subtask = document.getElementById(`list-content-edit-task${i}`).innerHTML;
+    let checkBox = document.getElementById(`checkbox-subtask-edit-task${i}`);
+    let checkBoxClickable = document.getElementById(`checkbox-subtask-edit-task-clickable${i}`);
+    
+    let emptyCheckBoxSrc = "img/empty-check-box.svg";
+    let filledCheckBoxSrc = "img/filled-check-box.svg";
+    
+    if (checkBoxClickable.src.endsWith(emptyCheckBoxSrc)) {
+        checkBoxClickable.src = filledCheckBoxSrc;
+        checkBox.src = filledCheckBoxSrc;
+        emptyTask.doneSubtasks.push(subtask);
+    } else {
+        checkBoxClickable.src = emptyCheckBoxSrc;
+        checkBox.src = emptyCheckBoxSrc;
+        let index = emptyTask.doneSubtasks.indexOf(subtask);
+        if (index !== -1) {
+            emptyTask.doneSubtasks.splice(index, 1);
+        }
+    }
+    console.log(emptyTask);
+}
+
+function newTitle() {
+    let title = document.getElementById("title-edit-task").value;
+    emptyTask.title = title;
+    console.log(emptyTask);
+}
+
+function newDescription() {
+    let description = document.getElementById("description-of-task-edit-task").value;
+    emptyTask.description = description;
+    console.log(emptyTask);
+}
+
+function newDate() {
+    let date = document.getElementById("date-edit-task").value;
+    emptyTask.deadline = date;
+    console.log(emptyTask);
+}
+
+function updateTask() {
+    let index = taskKeys.indexOf(emptyTask.id);
+    allTasks[index] = emptyTask;
+    console.log(allTasks);
 }
