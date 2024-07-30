714266fe1e127424d9ef0e5ae184034697c387bd
Patrick-Gogolin <patrick.gogolin@hotmail.com>
2024-07-30 18:11:35 +0200

    new commit

--- add-task-main.js
+++ add-task-main.js
@@ -54,3 +54,32 @@ function clearAddTaskMasque() {
     cancelAndConfirm.classList.add('d-none');
     resetAddTaskSlide(title, description, date, category, contactsContent, subtaskContent, addSubtaskInputfield)
 }
+
+function addSubtaskMain() {;
+    let addSignContainer = document.getElementById('add-subtask-svg-container');
+    let cancelAndConfirm = document.getElementById('cancel-or-confirm-subtask-container')
+    let inputField = document.getElementById('add-subtask-input-container-inputfield');
+    let content = document.getElementById('added-subtask-main-container');
+    subtasks.push(inputField.value);
+    content.innerHTML = "";
+    for (let i = 0; i < subtasks.length; i++) {
+        const subtask = subtasks[i];
+        content.innerHTML += renderAddedSubtasksHtmlMain(i, subtask);
+    }
+    inputField.value = '';
+    addSignContainer.classList.remove('d-none');
+    cancelAndConfirm.classList.add('d-none');
+    editMenuSubtaskIsOpen = false;
+}
+
+function openEditSubtaskMenuMain(i) {
+    let content = document.getElementById(`single-subtask-container${i}`);
+    let listValue = document.getElementById(`list-content${i}`).innerHTML;
+
+    if (editMenuSubtaskIsOpen === false) {
+        content.classList.remove('bg-grey-hover');
+        content.classList.add('blue-underline');
+        content.innerHTML = renderEditSubtaskHtmlMain(i, listValue);
+        editMenuSubtaskIsOpen = true;
+    }
+}
