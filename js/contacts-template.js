b5245c19c04a32862706b812687b7fed6b52796f
Patrick-Gogolin <patrick.gogolin@hotmail.com>
2024-08-04 15:36:37 +0200

    new commit

--- js/contacts-template.js
+++ js/contacts-template.js
@@ -10,3 +10,28 @@ function renderContactInfoSectionAfterDeleteContactHtml() {
       <div class="contact-info-header-separator-mobile"></div>
       </div>`;
 }
+
+function renderEditUserTemplateHtml(initials, activeUserInContacts) {
+    return /*html*/`
+    <div id="edit-user-popup-content" class="popup-content animation" onclick="doNotClose(event)">
+              <div class="popup-left">
+                  <div onclick="closePopup()" class="back-icon-white-boarder"><img class="back-icon-white" src="img/close_white.png" alt=""></div>
+                  <img class="join-logo" src="img/capa_2.png" alt="">
+                  <h1>Edit contact</h1>
+                  <div class="blue-line"></div>
+              </div>
+              <div id="user-logo" class="edit-contact-logo">${initials}</div>
+              <div class="popup-right">
+                  <div onclick="closePopup()" class="back-icon-boarder"><img class="back-icon" src="img/x.svg" alt=""></div>
+                  <form class="form" onsubmit="submitEditUserForm('${activeUserInContacts.id}'); return false;">
+                      <input id="editNameUser" class="add-contact-input-name" placeholder="Vor und Nachname" type="text" required>
+                      <input id="editMailUser" class="add-contact-input-mail" placeholder="Email" type="email" required>
+                      <input id="editPhoneUser" class="add-contact-input-tel" placeholder="Phone" type="tel" required>
+                      <div class="add-contact-form-buttons">
+                      <button type="button" class="cancel" onclick= "closePopup()">Close<img src="img/x.svg" alt=""></button>
+                      <button type="submit" class="create">Save<img src="img/check.png" alt=""></button>
+                      </div>
+                  </form>
+              </div>
+    </div>`;
+}
