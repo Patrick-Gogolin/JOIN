fc31069de5d3628b3b87504e77b7200abc0bf14f
Patrick-Gogolin <patrick.gogolin@hotmail.com>
2024-08-05 18:43:07 +0200

    sidebar.js js doc finished

--- templates/sidebar.js
+++ templates/sidebar.js
@@ -1,7 +1,13 @@
-// Funktion zur Navigation und Speichern der aktiven Seite
+/**
+ * Navigates to a specified page and stores the ID of the active page in local storage.
+ * 
+ * This function sets an item in local storage with the key 'activePage' and the value of the provided `id`.
+ * Then, it changes the current page to the specified `page` URL.
+ * 
+ * @param {string} page - The URL or path of the page to navigate to.
+ * @param {string} id - The identifier of the page to store in local storage as 'activePage'.
+ */
 function goToPage(page, id) {
-    // Speichere die aktive Element-ID in localStorage
     localStorage.setItem('activePage', id);
-    // Navigiere zur angegebenen Seite
     window.location.href = page;
 }
